c9cb1a95af2cec4c6c6f4849c23f3175971fcd2e
Matteo Rivola <ganesh86@hotmail.it>
2017-08-22 09:07:51 +0200

    second fix

--- media.py
+++ media.py
@@ -11,11 +11,21 @@ class Movie():
                  movie_storyline,
                  poster_image,
                  trailer_youtube):
-
+        """
+        Inizialize Instance of Movie Class
+        :param movie_title: string
+        :param movie_storyline: string
+        :param poster_image: string
+        :param trailer_youtube: string
+        """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """
+        Inizialize Instance for opening youtube trailer
+        :return: webbrowser to play video
+        """
         webbrowser.open(self.trailer_youtube_url)
